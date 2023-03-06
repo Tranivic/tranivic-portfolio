5c5b24116399f2fbd87315bc84568d5e9fa55621
Victor <victor.trani1@gmail.com>
2023-03-06 15:35:01 -0300

    feat: ui and slot components created (MainButton and SocialMedia)

---
+++ src/components/ui/SocialMedia/SocialMedia.vue
@@ -0,0 +1,46 @@
+<template>
+<ul class="social-media-menu">
+    <li>
+        <a href="https://github.com/Tranivic" target="_blank">
+            <i class="fa-brands fa-github"></i>
+        </a>
+    </li>
+    <li>
+        <a href="https://www.linkedin.com/in/victor-martins-t/" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+        </a>
+    </li>
+    <li>
+        <a href="https://www.youtube.com/channel/UCL-CI61gBpk5cMbhCZ9iwLw" target="_blank">
+            <i class="fa-brands fa-youtube"></i>
+        </a>
+
+    </li>
+    <li>
+        <a href="https://www.instagram.com/victortranii/" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+    </li>
+    <li>
+        <a href="mailto: victor.trani1@gmail.com" target="_blank">
+            <i class="fa fa-envelope"></i>
+        </a>
+
+    </li>
+    <li>
+        <a href="https://wa.me/5521987638760" target="_blank">
+            <i class="fa-brands fa-whatsapp"></i>
+        </a>
+    </li>
+</ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'SocialMedia.scss'
+</style>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import store from './store'
 
 // Global Components import (Slots)
 import MainButton from './components/slots/MainButton/MainButton.vue'
+import SocialMedia from './components/ui/SocialMedia/SocialMedia.vue'
 
 const app = createApp(App)
 app.use(store)
 app.use(router)
 app.component('main-button', MainButton)
+app.component('social-media', SocialMedia)
 app.mount('#app')
