e3b343cb5478fd3b0b95598fe744126a8cf77289
Victor <victor.trani1@gmail.com>
2023-03-06 15:34:07 -0300

    feat: footer finished

--- src/layouts/AppFooter/AppFooter.vue
+++ src/layouts/AppFooter/AppFooter.vue
@@ -8,6 +8,30 @@
         <main-button type="button" color="black">Schedule a call</main-button>
         <main-button type="button" color="white">Send me an email</main-button>
     </div>
+    <ul class="nav-menu-flat">
+        <li @click="toggleMenu" class="nav-menu-content-item">
+            <router-link to="/about">About</router-link>
+        </li>
+        <li @click="toggleMenu" class="nav-menu-content-item">
+            <router-link to="/contact">Contact</router-link>
+        </li>
+        <li @click="toggleMenu" class="nav-menu-content-item">
+            <router-link to="/work">Work</router-link>
+        </li>
+        <li @click="toggleMenu" class="nav-menu-content-item">
+            <a href="https://www.linkedin.com/in/victor-martins-t/" target="_blank">Linkedin</a>
+        </li>
+        <li @click="toggleMenu" class="nav-menu-content-item">
+            <a href="https://github.com/Tranivic" target="_blank">Github</a>
+        </li>
+    </ul>
+    <div class="social-media-container">
+        <social-media></social-media>
+    </div>
+    <div class="emoji-container">
+        <span>⚡</span>
+    </div>
+
 </footer>
 </template>
 
