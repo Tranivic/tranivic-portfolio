fc3c25c33b84a0665f8f223c6a092d9999fb0c2f
Victor <victor.trani1@gmail.com>
2023-03-07 21:41:33 -0300

    feat: HomeView structure started

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <section class="home-view-container">
     <animated-title firstLine="Hi, I’m Victor Trani." secondLine="I project, code and grow internet software."></animated-title>
+    <p class="sub-title">I’m a Digital Product Designer and Software Engineer who enjoys turning problems and<br> opportunities into simple interfaces through design and code.</p>
+    <social-media></social-media>
+    <main-button class="hire-btn" color="black">Hire me!</main-button>
   </section>
 </template>
 <script>
