dafab4220efb71120cececdb7e9ddf4ae78c5d2d
Victor <victor.trani1@gmail.com>
2023-03-08 23:02:54 -0300

    chore: code refactored for single component (removed from footer)

---
+++ src/components/ui/LetsTalk/LetsTalk.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="lets-talk-container">
+    <div class="talk-info">
+        <h1>Let's Talk about Your Project.</h1>
+        <h2>Something on your mind? Get in touch and let's chat!</h2>
+    </div>
+    <div class="footer-btns">
+        <main-button ref="calButton" data-cal-link="tranivic/15min" type="button" color="black">Schedule a call</main-button>
+        <main-button type="button" color="white">Send me an email</main-button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'LetsTalk.scss'
+</style>
