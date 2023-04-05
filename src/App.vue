f92f2074119cd0468bb746aec7d99a08f1880a06
Victor <victor.trani1@gmail.com>
2023-04-04 21:33:45 -0300

    fix: fixed soft loading effect (changed logic to template)

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,35 @@
 <template>
-<div v-if="isLoaded" class="page-loaded">
+  <template v-if="isLoaded">
     <app-header></app-header>
     <router-view />
     <app-footer></app-footer>
-</div>
-<div v-else class="page-loading"></div>
+  </template>
+  <div v-else class="page-loading"></div>
 </template>
 
 <script>
 import AppHeader from '@/layouts/AppHeader/AppHeader.vue';
 import AppFooter from '@/layouts/AppFooter/AppFooter.vue';
 export default {
-    async created() {
-        await this.$store.dispatch('integrateCalApi');
-        await this.$store.dispatch('fetchProjects');
-        await this.$store.dispatch('fetchLanguage');
-    },
-    mounted() {
-        addEventListener('load', (_) => {
-            this.isLoaded = true;
-        });
-    },
-    data() {
-        return {
-            isLoaded: false,
-        };
-    },
-    components: {
-        AppHeader,
-        AppFooter,
-    },
+  async created() {
+    await this.$store.dispatch('integrateCalApi');
+    await this.$store.dispatch('fetchProjects');
+    await this.$store.dispatch('fetchLanguage');
+  },
+  mounted() {
+    addEventListener('load', (_) => {
+      this.isLoaded = true;
+    });
+  },
+  data() {
+    return {
+      isLoaded: false,
+    };
+  },
+  components: {
+    AppHeader,
+    AppFooter,
+  },
 };
 </script>
 
@@ -38,13 +38,13 @@ export default {
 
 // Base app scss setups
 body {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 
-    #app {
-        width: 100%;
-    }
+  #app {
+    width: 100%;
+  }
 }
 </style>
