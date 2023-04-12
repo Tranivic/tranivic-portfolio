659547bd803c3b3795f5d39576f0d94c497173ea
Victor <victor.trani1@gmail.com>
2023-04-12 09:04:59 -0300

    refactored: better app.vue organization

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,35 @@
 <template>
-  <template v-if="isLoaded">
+<div v-if="isLoaded">
     <app-header></app-header>
     <router-view />
     <app-footer></app-footer>
-  </template>
-  <div v-else class="page-loading"></div>
+</div>
+<div v-else class="page-loading"></div>
 </template>
 
 <script>
 import AppHeader from '@/layouts/AppHeader/AppHeader.vue';
 import AppFooter from '@/layouts/AppFooter/AppFooter.vue';
 export default {
-  async created() {
-    await this.$store.dispatch('integrateCalApi');
-    await this.$store.dispatch('fetchProjects');
-    await this.$store.dispatch('fetchLanguage');
-  },
-  mounted() {
-    addEventListener('load', (_) => {
-      this.isLoaded = true;
-    });
-  },
-  data() {
-    return {
-      isLoaded: false,
-    };
-  },
-  components: {
-    AppHeader,
-    AppFooter,
-  },
+    async created() {
+        await this.$store.dispatch('integrateCalApi');
+        await this.$store.dispatch('fetchProjects');
+        await this.$store.dispatch('fetchLanguage');
+    },
+    mounted() {
+        addEventListener('load', (_) => {
+            this.isLoaded = true;
+        });
+    },
+    data() {
+        return {
+            isLoaded: false,
+        };
+    },
+    components: {
+        AppHeader,
+        AppFooter,
+    },
 };
 </script>
 
@@ -38,13 +38,13 @@ export default {
 
 // Base app scss setups
 body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 
-  #app {
-    width: 100%;
-  }
+    #app {
+        width: 100%;
+    }
 }
 </style>
