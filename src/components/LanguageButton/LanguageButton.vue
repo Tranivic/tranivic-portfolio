5791e24894e6dd0e00542107527ed722e1ccd3b2
Victor <victor.trani1@gmail.com>
2023-05-02 12:57:44 -0300

    feat: language buttom toolkit implemented

--- src/components/LanguageButton/LanguageButton.vue
+++ src/components/LanguageButton/LanguageButton.vue
@@ -1,31 +1,33 @@
 <template>
-    <div v-if="logoPath" class="language-changer-container">
-      <img alt="Language flag" width="30" :src="require(`@/assets/icons/${logoPath}`)" @click="changeLanguage" />
-    </div>
-  </template>
-  
-  <script>
-  export default {
+<div :data-text="toolKitText" v-if="logoPath" class="language-changer-container">
+    <img alt="Language flag" width="30" :src="require(`@/assets/icons/${logoPath}`)" @click="changeLanguage" />
+</div>
+</template>
+
+<script>
+export default {
     methods: {
-      async changeLanguage() {
-        await this.$store.dispatch('changeLanguage')
-        window.location.reload()
-      }
+        async changeLanguage() {
+            await this.$store.dispatch('changeLanguage')
+            window.location.reload()
+        }
     },
     computed: {
-      logoPath() {
-        const selectedLanguage = this.$store.getters.getSelectedLanguage
-        if (selectedLanguage === 'en') {
-            return 'brazil.svg'
-        } else {
-          return 'united-states-of-america.svg'
+        logoPath() {
+            const selectedLanguage = this.$store.getters.getSelectedLanguage
+            if (selectedLanguage === 'en') {
+                return 'brazil.svg'
+            } else {
+                return 'united-states-of-america.svg'
+            }
+        },
+        toolKitText() {
+            return this.$store.getters.getlanguageObject.toolkitLanguageSelect;
         }
-      }
     }
-  }
-  </script>
-  
-  <style lang="scss">
-  @import 'LanguageButton.scss';
-  </style>
-  
+}
+</script>
+
+<style lang="scss">
+@import 'LanguageButton.scss';
+</style>
