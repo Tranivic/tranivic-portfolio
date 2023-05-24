255527b8b85a80464bdfe1eda166b2ed57e7ea3b
Victor <victor.trani1@gmail.com>
2023-05-24 12:29:35 -0300

    refactor: social media component update/ add hashnode media

--- src/components/ui/SocialMedia/SocialMedia.vue
+++ src/components/ui/SocialMedia/SocialMedia.vue
@@ -1,35 +1,8 @@
 <template>
 <ul class="social-media-menu">
-    <li>
-        <a href="https://github.com/Tranivic" target="_blank">
-            <i class="interact-cursor fa-brands fa-github"></i>
-        </a>
-    </li>
-    <li>
-        <a href="https://www.linkedin.com/in/victor-martins-t/" target="_blank">
-            <i class="interact-cursor fa-brands fa-linkedin"></i>
-        </a>
-    </li>
-    <li>
-        <a href="https://www.youtube.com/channel/UCL-CI61gBpk5cMbhCZ9iwLw" target="_blank">
-            <i class="interact-cursor fa-brands fa-youtube"></i>
-        </a>
-
-    </li>
-    <li>
-        <a href="https://www.instagram.com/victortranii/" target="_blank">
-            <i class="interact-cursor fa-brands fa-instagram"></i>
-        </a>
-    </li>
-    <li>
-        <a href="mailto: victor.trani1@gmail.com" target="_blank">
-            <i class="interact-cursor fa fa-envelope"></i>
-        </a>
-
-    </li>
-    <li>
-        <a href="https://wa.me/5521987638760" target="_blank">
-            <i class="interact-cursor fa-brands fa-whatsapp"></i>
+    <li v-for="media in this.socialMedias" :key="media.name">
+        <a :href="media.link" target="_blank">
+            <i :class="media.iconIsBrands? 'fa-brands':'fa', media.iconName" class="interact-cursor"></i>
         </a>
     </li>
 </ul>
@@ -37,7 +10,54 @@
 
 <script>
 export default {
-
+    computed: {},
+    data() {
+        return {
+            socialMedias: [{
+                    name: 'Github',
+                    link: 'https://github.com/Tranivic',
+                    iconName: 'fa-github',
+                    iconIsBrands: true,
+                },
+                {
+                    name: 'Linkedin',
+                    link: 'https://www.linkedin.com/in/victor-martins-t/',
+                    iconName: 'fa-linkedin',
+                    iconIsBrands: true,
+                },
+                {
+                    name: 'Youtube',
+                    link: 'https://www.youtube.com/channel/UCL-CI61gBpk5cMbhCZ9iwLw',
+                    iconName: 'fa-youtube',
+                    iconIsBrands: true,
+                },
+                {
+                    name: 'Instagram',
+                    link: 'https://www.instagram.com/victortranii/',
+                    iconName: 'fa-instagram',
+                    iconIsBrands: true,
+                },
+                {
+                    name: 'Email',
+                    link: 'mailto: victor.trani1@gmail.com',
+                    iconName: 'fa-envelope',
+                    iconIsBrands: false,
+                },
+                {
+                    name: 'Whatsapp',
+                    link: 'https://wa.me/5521987638760',
+                    iconName: 'fa-whatsapp',
+                    iconIsBrands: true,
+                },
+                {
+                    name: 'Hashnode',
+                    link: 'https://victortrani.hashnode.dev/',
+                    iconName: 'fa-hashnode',
+                    iconIsBrands: true,
+                },
+            ]
+        }
+    },
 }
 </script>
 
