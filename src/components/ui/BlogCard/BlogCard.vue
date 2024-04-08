d5ed92430895d01a283b8679f70cc6a53bcd448e
Victor Trani <victor@wdhouse.com.br>
2024-04-08 16:19:44 -0300

    fix: fix blog card fetch to wait for data and display placeholder text

--- src/components/ui/BlogCard/BlogCard.vue
+++ src/components/ui/BlogCard/BlogCard.vue
@@ -1,27 +1,37 @@
 <template>
-    <template v-if="typeOfCard === 'complete'">
-        <li @click="pushToBlogPost" class="blog-card complete">
-            <header :style="{ backgroundImage: 'url(' + post.coverImage.url + ')' }"></header>
-            <footer>
-                <h1>{{ post.title }}.</h1>
-                <p>{{ post.brief }}</p>
-            </footer>
-        </li>
+    <template v-if="post">
+        <template v-if="typeOfCard === 'complete'">
+            <li @click="pushToBlogPost" class="blog-card complete">
+                <header :style="{ backgroundImage: 'url(' + post.coverImage.url + ')' }"></header>
+                <footer>
+                    <h1>{{ post.title }}.</h1>
+                    <p>{{ post.brief }}</p>
+                </footer>
+            </li>
+        </template>
+        <template v-else>
+            <li class="blog-card minimalist">
+                <header>
+                    <h1>{{ post.title }}.</h1>
+                    <p>{{ post.brief }}</p>
+                </header>
+                <footer>
+                    <main-button :isLink="false" @click="pushToBlogPost" class="read-btn">{{ this.readMoreBtnText }}</main-button>
+                    <div class="like-container">
+                        <i class="fas fa-book-open"></i>
+                        <span>{{ this.minOfReading(4) }}</span>
+                    </div>
+                </footer>
+            </li>
+        </template>
     </template>
     <template v-else>
         <li class="blog-card minimalist">
-            <header>
-                <h1>{{ post.title }}.</h1>
-                <p>{{ post.brief }}</p>
-            </header>
-            <footer>
-                <main-button :isLink="false" @click="pushToBlogPost" class="read-btn">{{ this.readMoreBtnText }}</main-button>
-                <div class="like-container">
-                    <i class="fas fa-book-open"></i>
-                    <span>{{ this.minOfReading(4) }}</span>
-                </div>
-            </footer>
-        </li>
+                <header>
+                    <h1>Waiting...</h1>
+                    <p>Waiting...</p>
+                </header>
+            </li>
     </template>
 </template>
 
